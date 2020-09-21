95657dc440623d0048d234c163b3fdfb1e69dbf9
Nick <igerster80@gmail.com>
2020-09-21 19:29:47 -0400

    More changes to html page

--- JS/htmlhide.js
+++ JS/htmlhide.js
@@ -1,8 +1,3 @@
-function TestsFunction() {
-    var T = document.getElementById("HTML"),
-        displayValue = "";
-    if (T.style.display == "")
-        displayValue = "none";
-
-    T.style.display = displayValue;
+function ShowHTML() {
+    document.getElementById("HTML").style.display = "";
 }
